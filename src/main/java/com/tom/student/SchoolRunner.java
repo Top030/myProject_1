05ef35fed4689d8f39415011e1cdd57127dd0f13
top030 <mini97202@gmail.com>
2019-06-22 21:17:09 +0800

    190622_1

--- src/main/java/com/tom/student/SchoolRunner.java
+++ src/main/java/com/tom/student/SchoolRunner.java
@@ -1,8 +1,18 @@
 package com.tom.student;
 
+import java.util.Scanner;
+
 public class SchoolRunner {
     public static void main(String[] args) {
-        Student stu= new Student("Tom",63,77);
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Please enter student's name:");
+        String name= scanner.next();
+        System.out.print("Please enter student's english:");
+        int english= scanner.nextInt();
+        System.out.print("Please enter student's math:");
+        int math= scanner.nextInt();
+
+        Student stu= new Student(name,english,math);
         stu.print();
     }
 }
--- src/main/kotlin/com/tom/student/Student.kt
+++ src/main/kotlin/com/tom/student/Student.kt
@@ -1,13 +1,23 @@
 package com.tom.student
 
+import java.util.*
+
 fun main(args: Array<String>) {
-    val stu= Student("Andy",89,67);
+    val scanner= Scanner(System.`in`);
+    print("Please enter student's name:");
+    var name= scanner.next();
+    print("Please enter student's english:");
+    var english= scanner.nextInt();
+    print("Please enter student's math:");
+    var math= scanner.nextInt();
+
+    val stu= Student(name,english,math);
     stu.print();
 }
 
 class Student(var name: String,var english: Int,var math: Int){
     fun print(){
-        var temp: String= name+ "\t"+ english+ "\t"+ math+ "\t"+ (english+math)/2
-        println(temp)
+        var temp: String= name+ "\t"+ english+ "\t"+ math+ "\t"+ (english+math)/2;
+        println(temp);
     }
 }
