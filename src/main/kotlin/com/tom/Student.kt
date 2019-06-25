921b21a18fe17f38bbaa3960bbaca67bf05e3d4c
top030 <mini97202@gmail.com>
2019-06-25 13:25:13 +0800

    190625_2

--- src/main/kotlin/com/tom/Student.kt
+++ src/main/kotlin/com/tom/Student.kt
@@ -25,25 +25,17 @@ private fun userInput() {
 
 class Student(var name: String?,var english: Int,var math: Int){
     fun print(){
-        var temp: String= name+ "\t"+ english+ "\t"+ math+ "\t"+ getAverage()+ "\t"+
-                if(getAverage()>= 60) "PASS" else "FAILED";
-        println(temp);
+        println("$name\t$english\t$math\t${getAverage()}\t${passOrFailed()}\t${grading()}");
     }
-    fun getAverage(): Int{
-        return (english+math)/2;
+    fun grading()= when(getAverage()){
+        in 90..100-> 'A';
+        in 80..89-> 'B';
+        in 70..79-> 'C';
+        in 60..69-> 'D';
+        else-> 'F';
     }
-    fun nameCheck(){
-        println(name?.length)
-    }
-    fun highest(): Int{
-//        var max= if(english>math){
-//            println("english");
-//            english
-//        }else{
-//            println("math")
-//            math
-//        }
-//        return max;
-        return (if(english> math) english else math);
-    }
+    fun passOrFailed()= if(getAverage()>= 60) "PASS" else "FAILED";
+    fun getAverage()= (english+math)/2;
+    fun nameCheck()= name?.length;
+    fun highest()= if(english> math) english else math;
 }
