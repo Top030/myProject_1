e2cf83e167f95ff14c00911851177ee762f9f815
top030 <mini97202@gmail.com>
2019-06-25 12:50:17 +0800

    190625_1

--- src/main/java/com/tom/student/Student.java
+++ src/main/java/com/tom/student/Student.java
@@ -13,13 +13,35 @@ public class Student {
     }
 
     public void print(){
-        String temp=  name+ "\t"+ english +"\t" +math +"\t"+ getAverage();
+        String temp=  name+ "\t"+ english +"\t" +math +"\t"+ getAverage()+
+                "\t"+ ((getAverage()>=60)? "PASS": "FAILED");
         System.out.print(temp);
-        if(getAverage()>= 60){
-            System.out.println("\tPASS");
-        }else{
-            System.out.println("\tFAILED");
+
+        int average= getAverage();
+        char grading= 'F';
+        switch(average/ 10){
+            case 10:
+            case 9:
+                grading= 'A';
+                break;
+            case 8:
+                grading= 'B';
+                break;
+            case 7:
+                grading= 'C';
+                break;
+            case 6:
+                grading= 'D';
+                break;
+            default:
+                grading='F';
         }
+        System.out.println("\t"+ grading);
+//        if(getAverage()>= 60){
+//            System.out.println("\tPASS");
+//        }else{
+//            System.out.println("\tFAILED");
+//        }
     }
 
     public int getAverage(){
