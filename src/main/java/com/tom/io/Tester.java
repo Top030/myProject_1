a73258749990ea9e6a65a78fa56a349a042a197d
top030 <mini97202@gmail.com>
2019-07-04 13:44:50 +0800

    190704_2

--- src/main/java/com/tom/io/Tester.java
+++ src/main/java/com/tom/io/Tester.java
@@ -1,20 +1,60 @@
 package com.tom.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
-        File file= new File("data1.txt");
+//        inputstream();
+//        filereader();
+        try {
+            BufferedReader br= new BufferedReader(new FileReader("data.txt"));
+            String line= br.readLine();
+            while(line!= null){
+                System.out.println(line);
+                line= br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void filereader() {
+        File file= new File("data.txt");
+        try {
+            FileReader fr= new FileReader(file);
+            int n= fr.read();
+            while(n!= -1){
+                System.out.print((char)n);
+                n= fr.read();
+            }
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void inputstream() {
+        File file= new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
         try {
-            FileInputStream is = new FileInputStream(file);
-        }catch(FileNotFoundException e){
+            FileInputStream fis= new FileInputStream(file);
+            int n= fis.read();
+            while(n!= -1){
+                System.out.print((char)n);
+                n= fis.read();
+            }
+            System.out.println();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.out.println("message");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
         System.out.println("file opened success");
     }
 }
